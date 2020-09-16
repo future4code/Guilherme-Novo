55881c558e288f2833a07ef1491b3030b19b6c67
unknown <guilhermenovo22@gmail.com>
2020-09-15 21:18:15 -0300

    Variáveis

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,53 @@
+
+// let name = "" // string, porque é uma letra e está com aspas.  
+// let idade = "" // string. Caso eu não coloque as aspas, valor no console é undefined.
+                // Se colocar um numeral é number.
+// console.log (typeof(name));
+// console.log (typeof(idade));
+
+//const name = "Guilherme"
+//let idade = 28
+//console.log (typeof(name)) //string
+//console.log (typeof(idade)) // number
+
+
+//const nome = "Guilherme"
+//let idade = 28
+//console.log ("Olá", nome, "você tem", idade, "anos");
+
+//let endereco = prompt ("1 - Qual seu endereço?")
+//console.log ("Resposta: Moro no Paraná");
+
+//let fruta = prompt ("2 - Qual sua fruta favorita?")
+//console.log ("Resposta: maça");
+
+//let esporte = prompt ("3 - Qual seu esporte favorito?")
+//console.log ("Resposta: Corrida");
+
+//let decepcao = prompt ("4 - Qual sua maior decepção?")
+//console.log ("Resposta: Ter o bolsonaro como presidente");
+
+//let covid = prompt ("5 - Qual gripe eu posso pegar?")
+//console.log ("Resposta: Coronga");
+
+//let food = ["Arroz", "feijão", "lasanha", "sushi", "libanesa", "carne"]
+//console.log("Essas são minhas comidas preferidas:")
+//console.log(food[0])
+//console.log(food[1])
+//console.log(food[2])
+//console.log(food[3])
+//console.log(food[4])
+//prompt ("Qual sua comida favorita?")
+//console.log(food[5])
+
+//let perguntas = ["Você já pensou no modelo de sociedade em nós vivemos", "Você acha que o capitalismo é o modelo ideal para o bem estar humano?", "Você se considera uma pessoa consciente sobre o modelo de estado?"]
+//let respostas = perguntas[0]
+//respostas = true
+//console.log(perguntas[0], respostas)
+//respostas = false
+//console.log(perguntas[1], respostas)
+//respostas = true
+//console.log(perguntas[2], respostas)
+
+
+
