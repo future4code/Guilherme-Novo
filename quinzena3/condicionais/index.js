726247672080a91d78aeb3845973ae167d33f521
unknown <guilhermenovo22@gmail.com>
2020-09-22 22:43:07 -0300

    Condicionais

---
+++ quinzena3/condicionais/index.js
@@ -0,0 +1,98 @@
+// console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+//Retorna o resto da divisão. Passam no teste os números pares. O numeros ímpares não passam.
+// Traz os preços das frutas que são colocadas no prompt; Caso uma fruta seja diferente dos casos ela custará 5 reais; A pêra custaria 5 reais casos tirasse o break;
+//Pedindo para o usuário digitar um número; Se fosse 10 a mensagem seria "Esse número passou no teste"; -10 daria erro; Um escopo filho consegue compartilhar os valores do escopo pai, o contrário não acontece. 
+
+// const idade = Number(prompt("Qual sua idade?"));
+
+//     if(idade >= 18) {
+//         console.log("Parabéns, você pode dirigir")
+//     }else {
+//         console.log("Você não pode dirigir")
+//     }
+
+// const diaDeEstudo = prompt("Qual seu turno de estudo. Digite  M (matutino) ou V (Vespertino) ou N (Noturno)");
+
+//     if (diaDeEstudo === "M") {
+//         console.log("Bom dia, flor do dia")
+//     } if (diaDeEstudo === "V") {
+//         console.log("Boa tarde!")
+//     }
+//     if (diaDeEstudo === "N") 
+//         console.log("Boa noite!");
+
+// const diaDeEstudo = prompt("Qual seu turno de estudo. Digite  M (matutino) ou V (Vespertino) ou N (Noturno)");
+
+//     switch (diaDeEstudo){
+//         case "M":
+//             console.log("Bom diannn!")
+//             break;
+//         case "V":
+//             console.log("Boa tarde!")
+//             break;
+//         case "N":
+//             console.log("Good Night!");
+//             break;    
+//         }
+
+// const filmeGenero = prompt("Qual gênero de filme você gosta?");
+// const valor = Number(prompt("Qual o valor do ingresso?"));
+
+//     if (filmeGenero === "fantasia" && valor <= 15){
+//         console.log("Bom filme!")
+//     }else {
+//         console.log("Escolha outro filme :/")
+//     }
+
+// const filmeGenero = prompt("Qual gênero de filme você gosta?");
+// const valor = Number(prompt("Qual o valor do ingresso?"));
+
+//     if (filmeGenero === "fantasia" && valor <= 15){
+//         let doces = prompt("Qual snack você quer comprar? Temos pipoca, chocolate e doce de leite");
+//                 if(doces === "pipoca", "chocolate", "doce de leite")
+//                     console.log("Bom filme!");
+//                     console.log("...com", doces); 
+//     }else {
+//         console.log("Escolha outro filme :/")
+//     }
+    
+
+    // console.log("--- Dados da compra ---")
+    // const nomeDoUsuário = prompt("Qual seu nome completo")
+    //     console.log("Nome do cliente:", nomeDoUsuário);
+    // const tipoDeJogo = prompt("Qual tipo de jogo? Digite IN caso seja internacional, ou DO caso seja doméstico. Lembrando que os jogos internacionais serão cobrados em dólar, na cotação de R$4,10");
+    //     if(tipoDeJogo === "IN")
+    //     console.log("Tipo de jogo: Internacional")
+    //     if(tipoDeJogo === "DO")
+    //     console.log("Tipo de jogo: Doméstico")
+    // const etapaDoJogo = prompt("Qual etapa do jogo? Digite SF para semifinal, DT para decisão terceiro lugar ou FI para final");
+    //     if(etapaDoJogo === "SF")
+    //     console.log("Etapa do Jogo: Semi-final")
+    //     if(etapaDoJogo === "DT")
+    //     console.log("Etapa do Jogo: Decisão terceiro lugar")
+    //     if(etapaDoJogo === "FI")
+    //     console.log("Etapa do Jogo: Final")
+    // const categoria = prompt("Digite 1, 2, 3 ou 4 para as categorias de ingressos")
+    //     if(categoria === 1)
+    //     if(categoria === 2)
+    //     if(categoria === 3)
+    //     if(categoria === 4)
+    //     console.log(categoria)
+    // let quantidadeDeIngressos = Number(prompt("Quantidade de ingressos?"));
+    //     if(quantidadeDeIngressos => 1)
+    //     console.log("Quantidade de ingressos:", quantidadeDeIngressos);
+    // console.log("--- Valor do ingresso ---");
+    //     if(etapaDoJogo === "SF" && categoria === 1 && quantidadeDeIngressos >= 1){
+    //         let valorDoIngresso = 132000 * quantidadeDeIngressos
+    //         console.log("valor do ingresso:", valorDoIngresso)
+    //     } 
+           
+        
+    //     if(etapaDoJogo === "SF" && categoria === 2)
+    //     console.log("R$880,00")
+    //     if(etapaDoJogo === "SF" && categoria === 3)
+    //         console.log("R$550,00")
+    //     if(etapaDoJogo === "SF" && categoria === 4)
+    //         console.log("R$220,00")
+    
