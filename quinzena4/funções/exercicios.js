9d52f58f5e1c10b37d3faddeedc434b03551789a
unknown <guilhermenovo22@gmail.com>
2020-10-01 13:41:04 -0300

    funções

---
+++ quinzena4/funções/exercicios.js
@@ -0,0 +1,106 @@
+// Exercício 1: 
+//     a) Será impresso os dois consoles, um com o valor 10 
+//     e outro com o valor 50.
+//     b) o processo ocorrerá normalmente, mas não aparecerá no console.
+
+
+// Exercício 2:
+//     a) Será impresso Darvas e goli, já que o 
+//     for tem como index não passar de 2 (<2)
+//      b) Será impresso Amanda e Caio, já que o for vai até dois. 
+
+// Exercício 3: 
+//  A função metodo armazena em um array o quadrado de numeros pares // Não entendi muito bem!
+ 
+// Exercício 4:
+  // a)
+  // 
+  // function informacoes(){
+  //   console.log("Oi, sou o Guilherme. tenho 28 anos e moro em Arapongas-Paraná e sou estudante")
+  // }
+  // informacoes();
+  
+  // b)   
+
+    // let informacoes = function(nome = "", idade = 0, cidade = "", estudante = true){
+    //     if(estudante === true){
+    //       estudante = "sou"
+    //     }else{
+    //       estudante = "não sou"
+    //   }
+    //   console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante} estudante.`)
+    
+    // }   
+
+    // let resultadoInformacoes = informacoes("Guilherme", 28, "Arapongas", true)
+    // console.log(resultadoInformacoes)
+    
+// Exercicio 5: 
+
+// a)
+
+  // function doisNumeros (num1, num2) {
+  //   const soma = num1 + num2
+  //   return soma;
+  // }
+
+  // const resultado = doisNumeros(3, 6)
+  // console.log(resultado)
+
+  // b) 
+
+    // function numeroDois (num1, num2){
+    //   const somaBoleano = num1 >= num2
+    //   return somaBoleano;
+    // }
+
+    // const resultado = numeroDois(1, 3)
+    // console.log(resultado)
+
+  // c) 
+
+    // function receber(mensagem) {
+    //   for (let i = 0; i < 10; i++){
+    //     console.log(mensagem)
+    //   }
+      
+    // }
+    // let receber1 = receber("Estou em pânico")
+    
+// Exercicio 6 
+
+  // a)
+
+  // const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+
+  // function arrayDeNumeros(arg) {
+  //   return arg;
+  // }
+
+  // console.log(arrayDeNumeros(array));
+
+  // b)
+
+  
+  // const numeroBoleano = function(arrayDeNumeros){
+  //   return arrayDeNumeros % 2 === 0;
+  // }
+        
+  // const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+  // const resultado = numeroBoleano(array);
+  // console.log(resultado);
+
+// c) 
+
+  // let numeros = function(arrayDeNumeros){
+  //   return arrayDeNumeros.length
+  // }
+
+  // const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22]
+  // const resultado = numeros(array)
+  // console.log(resultado);
+
+//  d)
+
+
+
